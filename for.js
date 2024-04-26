d7db6fb33f64538b7e977851c1cf1babb67aaf65
Matheus Alves <161778574+Matheus-V-Alves@users.noreply.github.com>
2024-04-25 21:00:27 -0300

    WHLE-FOR

---
+++ for.js
@@ -0,0 +1,11 @@
+function calcular(){
+var numero= document.getElementById.value 
+
+    for (i=numero; i<10; i%=0){
+        console.log(i)
+        
+
+    }
+    `A soma do ${numero} é 10`
+
+}
---
+++ while.js
@@ -0,0 +1,8 @@
+function calcular(){
+    var numero= document.getElementById.value 
+
+    while(numero > 0 ){
+        console.log(numero)
+        numero--
+    }
+}
